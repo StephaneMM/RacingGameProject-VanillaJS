cd8d372818481fd579332c10a89177c82ba11835
Stephane Mbesse <stephane.mbesse@gmail.com>
2021-04-27 15:59:21 +0200

    guillaume review

--- js/index.js
+++ js/index.js
@@ -1,87 +1,88 @@
-let myCar = document.querySelector(".my-car")
+let myCar = document.querySelector(".my-car");
 let swerveBy = 50;
-let cars = document.querySelectorAll(".cars")
-let game = document.querySelector(".game-div")
+let cars = document.querySelectorAll(".cars");
+let game = document.querySelector(".game-div");
 
 function driving(e) {
-    if (e.key === 'ArrowLeft') {
-        myCar.style.left = `${parseInt(myCar.style.left) - swerveBy}px`;
-    }
+  if (e.key === "ArrowLeft") {
+    console.log("here", myCar);
+    myCar.style.left = `${parseInt(myCar.style.left) - swerveBy}px`;
+  }
+  //   console.log(">>>", e.key);
+  //   console.log(parseInt(myCar.style.left));
+  //   console.log(`${parseInt(myCar.style.left) - swerveBy}px`);
 
-    if (e.key === 'ArrowRight') {
-        myCar.style.left = `${parseInt(myCar.style.left) + swerveBy}px`;
-    }
+  if (e.key === "ArrowRight") {
+    myCar.style.left = `${parseInt(myCar.style.left) + swerveBy}px`;
+  }
 }
 
-document.addEventListener('keydown', driving);
-
-let rdmIA = 0;
-let rdmIB = 0;
+document.addEventListener("keydown", driving);
 
 function create2Nb() {
-rdmIA = parseInt(Math.floor(Math.random() * 3));
-rdmIB = parseInt(Math.floor(Math.random() * 3));
+  let rdmIA = parseInt(Math.floor(Math.random() * 3));
+  let rdmIB = parseInt(Math.floor(Math.random() * 3));
 
-while (rdmIA === rdmIB){
+  while (rdmIA === rdmIB) {
     rdmIB = parseInt(Math.floor(Math.random() * 3));
-}  
+  }
+  return [rdmIA, rdmIB];
 }
-
-
 function creatingSpace(index) {
+  // console.log("current ind x", index)
+  const newCar = document.createElement("div");
+  newCar.className = "cars ";
+  newCar.className += index === 0 ? "left" : index === 1 ? "middle" : "right";
+  game.appendChild(newCar);
 
-    switch(index){
-        case 0:
-            game.innerHTML+= `<div class="cars left go"></div>`;
-            break;
-        
-        case 1:
-            game.innerHTML+= `<div class="cars middle go"></div>`;
-            break;
-
-        case 2:
-            game.innerHTML+= `<div class="cars right go"></div>`;
-            break;
-    }
-    
+  //const timeoutId = setTimeout(() => {
+      newCar.className += " go";
+      // clearTimeout(timeoutId);
+  //}, 1000)
 }
 
-function clearHTML() {
-    game.innerHTML=`<div class="my-car" style="left: 175px"></div>`
+// function clearHTML() {
+//   // game.innerHTML = `<div class="my-car" style="left: 175px"></div>`;
+// }
 
-    
-}
+/*let salve = setInterval(() => {
+  const [a, b] = create2Nb();
+  // console.log(a, b);
+  creatingSpace(a);
+  creatingSpace(b);
+}, 1000);*/
 
+let _FRAMEID;
+let tick = 0;
 
+const gameLoop = () => {
+  const [a, b] = create2Nb();
+  // console.log(a, b);
+  if (tick % 60 === 0) {
+    creatingSpace(a);
+    creatingSpace(b);
+  }
 
+  tick++;
+  _FRAMEID = requestAnimationFrame(gameLoop);
+};
 
+_FRAMEID = requestAnimationFrame(gameLoop);
+//cancelAnimationFrame(_FRAMEID)
 
-let salve = setInterval(() => {
-    create2Nb();
-    creatingSpace(rdmIA);
-    creatingSpace(rdmIB);
+//  let salve2=setTimeout(function() {
 
- }, 1000)
+//     setInterval(() => {
+//     create2Nb();
+//     creatingSpace(rdmIA);
+//     creatingSpace(rdmIB);
 
- setTimeout(function() {
-    
-        setInterval(() => {
-        create2Nb();
-        creatingSpace(rdmIA);
-        creatingSpace(rdmIB);
-    
-     }, 1000)
-}, 500)
+//  }, 1000)
+// }, 500)
 
-
-
-
- let clear = setInterval(()=> { 
-     clearHTML()
-     }, 999);
-
- 
-
+// let clear = setInterval(() => {
+//   clearHTML();
+// }, 999);
 
 // console.log(EVERY STEP !!!!!);
 // console.log(EVERY STEP !!!!!);
